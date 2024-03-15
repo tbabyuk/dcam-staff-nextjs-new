eb5fcdeb4ccb1b03aa9c21643054c8abaad37642
Terry Babyuk <tbabyuk@gmail.com>
2024-03-15 03:05:44 -0400

    improved payday logic in check-submit api again

--- app/api/check-submit/route.js
+++ app/api/check-submit/route.js
@@ -7,17 +7,16 @@ export const POST = async (request) => {
     
     const {teacher, closestPaydayUnformatted} = await request.json()
 
-    console.log("logging closestPayday from check-submit API", closestPaydayUnformatted, teacher)
+    console.log("logging closestPaydayUnformatted from check-submit API", closestPaydayUnformatted, teacher)
 
     try {
         await connectToStaffDB()
         const {payday} = await Meta.findOne({"teacher": teacher.toLowerCase()}, {_id: 0, payday: 1})
 
         // When closestPayday is bigger than the payday last submitted, reset all attendance and update the payday field with the closest payday
-        
-        console.log("loggin closestPayday from 'check-submit' API", closestPaydayUnformatted)
-
         // const testDate = new Date("2024-03-05T05:00:00.000Z")
+
+        console.log("LOgging payday as it is in mongoDB:", payday)
 
         if (new Date(closestPaydayUnformatted) > new Date(payday)) {
             await Meta.updateOne({"teacher": teacher.toLowerCase()}, {$set: {"week1Submitted": false, "week2Submitted": false, "payday": new Date(closestPaydayUnformatted), "totalPay": 0, "notifyEmailSent": false}})
