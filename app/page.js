0da51fe383037a6e1a83a5bed6b0a9e2bf0f2eec
Terry Babyuk <tbabyuk@gmail.com>
2024-03-18 01:38:35 -0400

    changed login page styling to center the logo

--- app/page.js
+++ app/page.js
@@ -18,9 +18,9 @@ const Home = async () =>  {
 
   return (
     <main className="flex h-screen">
-      <div className="w-full md:w-1/2 bg-[url('/home_bg_left.jpg')] bg-cover grid place-items-center">
+      <div className="w-full md:w-1/2 bg-[url('/home_bg_left.jpg')] bg-cover">
           {/* <Link href="/api/auth/signin?callbackUrl=/welcome" className="sign-in-btn">SIGN IN</Link> */}
-          <img src="dcam-logo-white-long.png" width={200} className="absolute top-12" />
+          <img src="dcam-logo-white-long.png" width={200} className="mx-auto my-16" />
           <LoginForm />
       </div>
       <div className="hidden md:block w-1/2 bg-[url('/home_bg_right.jpg')] bg-cover" />
