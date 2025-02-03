809f99de83fda7f68351bbfaa22d6d3e0ea7811a
Terry Babyuk <tbabyuk@gmail.com>
2025-02-03 12:38:30 -0500

    updated login page title to 'Teacher Portal Login'

--- app/components/LoginForm.jsx
+++ app/components/LoginForm.jsx
@@ -46,7 +46,7 @@ export const LoginForm = () => {
 
   return (
         <form className="flex flex-col w-[330px] bg-black bg-opacity-30 text-gray-100 rounded-md mx-auto px-10 py-8" onSubmit={handleLogin}>
-            <h2 className="text-xl text-center mt-2 mb-6">Staff Login</h2>
+            <h2 className="text-xl text-center mt-2 mb-6">Teacher Portal Login</h2>
             <label>
                 <span className="block mb-1">Email:</span>
                 <input 
