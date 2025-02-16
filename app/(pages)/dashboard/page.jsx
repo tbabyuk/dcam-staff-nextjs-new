f593d1061ac8580072572da7fb727095ccb90c72
Terry Babyuk <tbabyuk@gmail.com>
2025-02-15 21:38:33 -0500

    removed submit reset logic from Dashboard page

--- app/(pages)/dashboard/page.jsx
+++ app/(pages)/dashboard/page.jsx
@@ -1,49 +1,11 @@
 "use client"
 
 import { useSession } from "next-auth/react"
-import { useEffect } from "react"
-import { usePayday } from "@/app/hooks/usePayday"
-
 
 
 const DashboardPage = () => {
 
   const {data: session} = useSession()
-  const {closestPaydayUnformatted} = usePayday()
-
-  console.log("from welcome page, logging closestPaydayUnformatted", closestPaydayUnformatted)
-
-
-  useEffect(() => {
-    
-    const checkDateSubmitted = async () => {
-
-      console.log("checkDateSubmitted FIRED")
-
-      if (closestPaydayUnformatted) {
-
-        try {
-          const res = await fetch("/api/check-submit", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({teacher: session.user.name, closestPaydayUnformatted})
-        })
-  
-        const {message} = await res.json()
-  
-        console.log("from check-submit API call:", message)
-  
-        } catch (error) {
-          console.log("An error occured while checking DateSubmitted")
-        }
-      }
-    }
-
-    checkDateSubmitted()
-
-  }, [session])
 
 
   return (
